c820cbab010fdadac0109cd5a0e6938a7f234596
MarionD-23 <74351661+MarionD-23@users.noreply.github.com>
2020-11-16 11:33:41 +0100

    homepage added
    first version of homepage html & css

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href=styles.css />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js"></script>
+</head>
+<body>
+    <header class="banner">
+        <div class="menu-box">
+            <img src="./assets/images/logo.png" alt="logo natureemoi" class="logo">
+            <nav class="menu">
+                <li>Accueil</li>
+                <li>Nos meilleurs ventes</li>
+                <li>Nos plantes</li>
+            </nav>
+        </div>
+        <div class="title">
+            <h1>Nature Emoi, meilleur que le chocolat.</h1>
+            <p>Scroll</p>
+        </div>
+    </header>
+    <div class="services">
+        <ul class="services-list">
+            <li>
+                <i class="fas fa-store delivery-icon"></i>
+                <p>Nos magasins à votre services</p>
+            </li>
+            <li>
+                <i class="fas fa-people-carry delivery-icon"></i>
+                <p>Retrait en magasin sans contact</p>
+            </li>
+            <li>
+                <i class="fas fa-truck delivery-icon"></i>
+                <p>Livraison à domicile sans contact</p>
+            </li>
+        </ul>
+    </div>
+    <div class="best-sells">
+        <h1>Nos meilleures ventes</h1>
+     
+        <div class="product-list">
+            <a class="product product1 no-grid">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>            
+                </div>
+            </a>
+            <a class="product product2 no-grid">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>  
+                </div>
+            </a>
+            <a class="product product3 no-grid">
+                <div class="informations">                
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>   
+                </div>
+            </a>
+            <a class="product product4 no-grid">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>  
+                </div>
+            </a>   
+        </div>
+    </div>
+
+    <div class="plants">
+        <h1>Nos plantes</h1>
+        <div class="plant-list">
+            <a class="plant plant1">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>            
+                </div>
+            </a>
+            <a class="plant plant2">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>  
+                </div>
+            </a>
+            <a class="plant plant3">
+                <div class="informations">                
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>   
+                </div>
+            </a>
+            <a class="plant plant4">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>  
+                </div>
+            </a>
+            <a class="plant plant5">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>  
+                </div>
+            </a>
+            <a class="plant plant6">
+                <div class="informations">
+                    <p class="name">Plante</p>
+                    <p class="price">12,99</p>  
+                </div>
+            </a>   
+        </div>
+    </div>
+    <footer>
+        <p>2020 - Nature Emoi</p>
+        <p>&copy; Conditions générales de ventes</p>
+    </footer>
+</body>
+</html>
